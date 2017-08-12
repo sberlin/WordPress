8fe2c26052454c23c02299c426fdbe82335cb04e
sberlin <sberlin@users.noreply.github.com>
2017-08-12 22:35:09 +0200

    Show only titles on category pages
    Improves performance when loading posts with many images
    
    Uses original content.php

--- wp-content/themes/twentythirteen/category.php
+++ wp-content/themes/twentythirteen/category.php
@@ -25,7 +25,7 @@ get_header(); ?>
 
 			<?php /* The loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
+				<?php get_template_part( 'content', 'title' ); ?>
 			<?php endwhile; ?>
 
 			<?php twentythirteen_paging_nav(); ?>
---
+++ wp-content/themes/twentythirteen/content-title.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The default template for displaying titles
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 2.2
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php if ( has_post_thumbnail() && ! post_password_required() && ! is_attachment() ) : ?>
+		<div class="entry-thumbnail">
+			<?php the_post_thumbnail(); ?>
+		</div>
+		<?php endif; ?>
+
+		<?php if ( is_single() ) : ?>
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<?php else : ?>
+		<h1 class="entry-title">
+			<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+		</h1>
+		<?php endif; // is_single() ?>
+
+		<div class="entry-meta">
+			<?php twentythirteen_entry_meta(); ?>
+			<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+</article><!-- #post -->
